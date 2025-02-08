c66ac70895947cb16cdd265776519b3de267e43c
Ubaid <ubaidwazir2003@gmail.com>
2025-02-08 09:47:44 +0500

    updated code

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -18,6 +18,7 @@ export class APIError extends Error {
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_AUTH_SERVER_URL?.replace(/\/api\/?$/, '');
 
+
 export async function fetchAPI(
   endpoint: string,
   options: FetchOptions = {}
